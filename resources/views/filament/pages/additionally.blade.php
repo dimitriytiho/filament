7890b6a293c2fae3dc8db08302966004445701a0
Dmitriy <dimitriytiho@gmail.com>
2023-10-13 07:02:23 +0300

    Views elements.

--- resources/views/filament/pages/additionally.blade.php
+++ resources/views/filament/pages/additionally.blade.php
@@ -4,4 +4,59 @@
         {{ $this->form }}
         <button type="submit">Save</button>
     </form>--}}
+
+    <x-filament::button
+        color="warning"
+        size="xl"
+        icon="heroicon-m-sparkles"
+        icon-position="after"
+        tooltip="My Button"
+        outlined="true"
+    >
+        My Button
+        <x-slot name="badge">77</x-slot>
+    </x-filament::button>
+
+    <x-filament::badge
+        color="success"
+    >
+        New
+    </x-filament::badge>
+
+    <x-filament::icon-button
+        icon="heroicon-m-plus"
+        color="gray"
+        label="New label"
+    />
+
+    <x-filament::fieldset>
+        <x-slot name="label">
+            Address
+        </x-slot>
+        Lorem ipsum dolor sit amet.
+    </x-filament::fieldset>
+
+    <x-filament::section>
+        <x-slot name="heading">
+            User details
+        </x-slot>
+
+        <x-slot name="description">
+            This is all the information we hold about the user.
+        </x-slot>
+
+        lorem5
+    </x-filament::section>
+
+    <x-filament::section
+        collapsible
+        collapsed
+    >
+        <x-slot name="heading">
+            User details
+        </x-slot>
+
+        Lorem ipsum dolor sit amet, consectetur.
+    </x-filament::section>
+
 </x-filament-panels::page>
