a8e13627405fb6d0f961630f120d4a00693553c7
Dmitriy <dimitriytiho@gmail.com>
2024-02-22 15:46:22 +0300

    Edit ArrHelper.

--- app/Helpers/ArrHelper.php
+++ app/Helpers/ArrHelper.php
@@ -5,16 +5,17 @@ namespace App\Helpers;
 class ArrHelper
 {
     /**
-     * Возвращает массив в который добавляет значения из ключей.
+     * Возвращает массив в который добавляет в ключи значения.
      *
      * @param array $arr
+     * @param bool $translate
      * @return array
      */
-    public static function valInKey(array $arr): array
+    public static function valInKey(array $arr, bool $translate = false): array
     {
         $res = [];
         foreach ($arr as $val) {
-            $res[$val] = $val;
+            $res[$val] = $translate ? __($val) : $val;
         }
         return $res;
     }
