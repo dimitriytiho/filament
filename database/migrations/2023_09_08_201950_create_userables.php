80d14c350e5fe8743976dff21d124ce26fa5a3b1
Dmitriy <dimitriytiho@gmail.com>
2024-02-29 19:06:20 +0300

    User polymorf migration.

---
+++ database/migrations/2023_09_08_201950_create_userables.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Permission;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('userables', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete();
+            $table->morphs('userable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('userables');
+    }
+};
