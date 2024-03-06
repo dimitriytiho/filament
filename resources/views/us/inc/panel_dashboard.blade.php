f02fd54de3a34bdcced8a41dfe9dc2b7a0bb76ec
Dmitriy <dimitriytiho@gmail.com>
2024-03-06 11:38:43 +0300

    Panel dashboard in public site, add  target blanc.

--- resources/views/us/inc/panel_dashboard.blade.php
+++ resources/views/us/inc/panel_dashboard.blade.php
@@ -1,10 +1,10 @@
 @if(auth()->check() && auth()->user()->isAdmin())
     <section class="bg-dark py-1 px-2">
-        <a href="{{ session()->get('back_link_admin', route('filament.' . config('filament.slug') . '.pages.dashboard')) }}" class="me-3" title="@lang('dashboard')">
+        <a href="{{ session()->get('back_link_admin', route('filament.' . config('filament.slug') . '.pages.dashboard')) }}" class="me-3" title="@lang('dashboard')" target="_blank">
             <i class="fas fa-tachometer-alt"></i>
         </a>
         @if(!empty($id) && !empty($kebab) && Route::has('filament.' . config('filament.slug') . ".resources.{$kebab}.edit"))
-            <a href="{{ route('filament.' . config('filament.slug') . ".resources.{$kebab}.edit", $id) }}" class="me-3" target="_blank" title="@lang('edit')">
+            <a href="{{ route('filament.' . config('filament.slug') . ".resources.{$kebab}.edit", $id) }}" class="me-3" title="@lang('edit')" target="_blank">
                 <i class="fas fa-edit"></i>
             </a>
         @endif
