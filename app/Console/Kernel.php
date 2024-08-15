6b2868539e45cd4e376f4e04ba5f90ac93515ab3
Dmitriy <dimitriytiho@gmail.com>
2024-08-15 11:51:41 +0300

    Health schedule.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Spatie\Health\Commands\{DispatchQueueCheckJobsCommand, RunHealthChecksCommand, ScheduleCheckHeartbeatCommand};
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +13,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // Health checks
+        $schedule->command(RunHealthChecksCommand::class)->everyFiveMinutes();
+        $schedule->command(ScheduleCheckHeartbeatCommand::class)->everyFiveMinutes();
+        $schedule->command(DispatchQueueCheckJobsCommand::class)->everyFiveMinutes();
     }
 
     /**
